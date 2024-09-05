2906b5bf22c0479743cc1bfe76b6c7368fc65e32
jona <a@b.c>
2024-09-05 03:07:28 +0200

    checkpoint every 10 steps

--- benchmark/utils/get_callbacks.py
+++ benchmark/utils/get_callbacks.py
@@ -1,5 +1,44 @@
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, Callback, TQDMProgressBar
 
+import pytorch_lightning as pl
+import os
+
+class CheckpointEveryNSteps(pl.Callback):
+    """
+    Save a checkpoint every N steps, instead of Lightning's default that checkpoints
+    based on validation loss.
+    """
+
+    def __init__(
+        self,
+        save_step_frequency,
+        prefix="N-Step-Checkpoint",
+        use_modelcheckpoint_filename=False,
+    ):
+        """
+        Args:
+            save_step_frequency: how often to save in steps
+            prefix: add a prefix to the name, only used if
+                use_modelcheckpoint_filename=False
+            use_modelcheckpoint_filename: just use the ModelCheckpoint callback's
+                default filename, don't use ours.
+        """
+        self.save_step_frequency = save_step_frequency
+        self.prefix = prefix
+        self.use_modelcheckpoint_filename = use_modelcheckpoint_filename
+
+    def on_train_batch_end(self, trainer: pl.Trainer, *args):
+        """ Check if we should save a checkpoint after every train batch """
+        epoch = trainer.current_epoch
+        global_step = trainer.global_step
+        if global_step % self.save_step_frequency == 0:
+            if self.use_modelcheckpoint_filename:
+                filename = trainer.checkpoint_callback.filename
+            else:
+                filename = f"{self.prefix}_{epoch=}_{global_step=}.ckpt"
+            ckpt_path = os.path.join(trainer.checkpoint_callback.dirpath, filename)
+            trainer.save_checkpoint(ckpt_path)
+
 def get_callbacks(args):
     monitor_mode = 'min' if 'loss' in args.logger.monitor else 'max'
     checkpoint_naming = '{epoch}-{step}-{valid_loss:.4f}'
@@ -16,5 +55,5 @@ def get_callbacks(args):
         patience=args.scheduler.earlystop_patience, 
         mode=monitor_mode, 
         verbose=True)
-    return [checkpoint_callback, early_stop_callback, TQDMProgressBar(refresh_rate=10)]
+    return [checkpoint_callback, early_stop_callback, TQDMProgressBar(refresh_rate=1), CheckpointEveryNSteps(10)]
 
